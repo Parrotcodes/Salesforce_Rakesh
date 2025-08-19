3c303225c401174465e7b4db3b3b208fdd849e60
Rakesh <parrotrakhy@gmail.com>
2025-08-19 16:34:46 +0530

    week2 assignment done! Concept: Exception-Handling

---
+++ PracticeMain/JavaPracticeCode/src/AdvConcepts/OtherConcepts/Threads/SynchronizedThreadController.java
@@ -0,0 +1,61 @@
+package AdvConcepts.OtherConcepts.Threads;
+
+//synchronized method will handle the multiple threads running simultaneously and one will run and one more thread will be in waiting stage
+
+import java.util.Comparator;
+
+class Counter{
+     int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
+
+//class NewThread implements Runnable{
+//    public void run(){
+//        for (int i = 0; i < 1000; i++) {
+//            Counter.increment();
+//        }
+//    }
+//}
+
+//class AnotherThread implements Runnable{
+//    public void run(){
+//        for (int i = 0; i < 1000; i++) {
+//            Counter.increment();
+//        }
+//    }
+//}
+
+public class SynchronizedThreadController {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+
+
+        Runnable ot1 = ()-> {
+            for (int i = 0; i < 1000; i++) {
+                c.increment();
+            }
+        };
+
+        Runnable ot2 = ()-> {
+            for (int i = 0; i < 1000; i++) {
+                c.increment();
+            }
+        };
+
+        Thread nt1 =  new Thread(ot1);
+        Thread nt2 = new Thread(ot2);
+
+       nt1.start();
+       nt2.start();
+
+        nt1.join();
+        nt2.join();
+
+        System.out.println(c.count);
+
+
+
+    }
+}
--- WeekWsieTraining/src/Week2/Day5/Assignment/FindTypeOfException.java
+++ WeekWsieTraining/src/Week2/Day5/Assignment/FindTypeOfException.java
@@ -2,7 +2,16 @@ package Week2.Day5.Assignment;
 
 import java.util.regex.Pattern;
 
+//PatternSyntaxException
+//First new MyClass() call → ExceptionInInitializerError caused by PatternSyntaxException.
+//Second new MyClass() call → NoClassDefFoundError.
+// the class failed initialization earlier. Once it fails, it won’t attempt re-initialization again.
+//   JVM marks it as unusable. So,Java Throws: NoClassDefFoundError
+
+//Error: ExceptionInInitializerError: Exception java.util.regex.PatternSyntaxException [in thread "main"]
+
 class MyClass {
+    //PatternSyntaxException
     private static Pattern PATT = Pattern.compile("(");
 }
 
@@ -14,7 +23,7 @@ public class FindTypeOfException {
 
         } catch (Throwable t) {}
 
-
+//ClassNotFoundError
         new MyClass();
 
     }
