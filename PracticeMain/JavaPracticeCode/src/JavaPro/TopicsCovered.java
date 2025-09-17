2776995f67d8c13319ce0632f88166539cfad5e1
Rakesh <parrotrakhy@gmail.com>
2025-09-17 12:27:13 +0530

    updated JavaPro package added info what are the topics covered and branches realted to those topics

---
+++ PracticeMain/JavaPracticeCode/src/JavaPro/TopicsCovered.java
@@ -0,0 +1,19 @@
+package JavaPro;
+
+public class TopicsCovered {
+//    Junit test cases files -- available in new branch (week4_junit) and in master branch (PracticeMain/src/UnitTesting)
+
+//    JUint5
+//    JSP
+//    JDBC + SERVLET + ServletFilter
+//    JSTL
+//    Session mangaement + reqDispatch
+//    Login System (Handled back button)
+
+//    FullStack Dynamic WebPage (JSP + JSTL + JDBC + Servlet + MySQL + HTML/CSS/JS + AJAX)
+//              Followed: MVC concept - model,view,Controller
+//                          model - daoClass (db connection, daoUser class)
+//                          Controller - Servlet
+//                          view - JSP web
+
+}
